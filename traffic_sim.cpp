f4e3ebaa39e447d14d715bac875f96383b2b85ad
ardabakir <ardabakir1@gmail.com>
2021-12-22 19:28:28 +0300

    add north and south threads

--- traffic_sim.cpp
+++ traffic_sim.cpp
@@ -13,13 +13,19 @@
 pthread_t laneThreads[NUM_THREADS-1];
 pthread_t policeThread;
 pthread_mutex_t street;
+pthread_mutex_t cars;
 pthread_cond_t condition;
 pthread_attr_t attributes;
 
 enum lane{NORTH,EAST,SOUTH,WEST}; // 0,1,2,3
 
+int carID;
+
 struct Car{
+    int id;
     time_t arrival;
+    time_t departure;
+    time_t wait;
     lane lane;
 };
 
@@ -36,7 +42,7 @@ using namespace std;
 int main(int argc, char **argv){
     srand((int)time(0));
     void* status;
-
+    carID = 0;
     int sec = 0;
     double duration = 0;
     time_t startTime = time(0);
@@ -53,76 +59,120 @@ int main(int argc, char **argv){
             probability = strtod(temp,NULL);
         }
     }
-    Car car;
-    eastQueue.push(car);
-    westQueue.push(car);
+    Car car0;
+    car0.id=carID;
+    car0.lane = NORTH;
+    car0.arrival = time(0);
+    northQueue.push(car0);
+    carID++;
+    
+    Car car1;
+    car1.id=carID;
+    car1.lane = EAST;
+    car1.arrival = time(0);
+    eastQueue.push(car1);
+    carID++;
+
+    Car car2;
+    car2.id=carID;
+    car2.lane = SOUTH;
+    car2.arrival = time(0);
+    southQueue.push(car2);
+    carID++;
+    
+    Car car3;
+    car3.id=carID;
+    car3.lane = WEST;
+    car3.arrival = time(0);
+    westQueue.push(car3);
+    carID++;
+
 
     cout << "sim_time: " << sim_time << endl;
     cout << "probability: " << probability << endl;
 
-    pthread_create(&laneThreads[1],NULL,defaultLane,(void *)EAST);
-    pthread_create(&laneThreads[3],NULL,defaultLane,(void *)WEST);
     pthread_create(&policeThread,NULL,police,NULL);
+    pthread_create(&laneThreads[0],NULL,defaultLane,(void *)NORTH);
+    pthread_create(&laneThreads[1],NULL,defaultLane,(void *)EAST);
+    pthread_create(&laneThreads[2],NULL,defaultLane,(void *)SOUTH);
+    pthread_create(&laneThreads[3],NULL,defaultLane,(void *)WEST);
+    
 
     while(duration < sim_time){
         pthread_mutex_lock(&street);
         if( clock() - prev_sec > CLOCKS_PER_SEC) {
             prev_sec = ++sec * CLOCKS_PER_SEC; 
             cout << "one second passed" <<endl;
+            cout<<"   "<<northQueue.size()<<endl;
+            cout<<westQueue.size()<<"      "<<eastQueue.size()<<endl;
+            cout<<"   "<<southQueue.size()<<endl;
         }
+        
 
         duration = (clock() - start ) / (double) CLOCKS_PER_SEC;
         pthread_mutex_unlock(&street);
     }
 
-    
     exit(0);
     //pthread_sleep(15);
 
-    // time_t end;
-    // time(&end);
-
-    // cout<<"duration: "<< difftime(end,start) <<endl;
+    
 
     return 0;
 }
 
 void addCar(int lane){
     Car car;
-    //double random = (double)rand() / (double)RAND_MAX;
-    
+    pthread_mutex_lock(&cars);
+    car.id = carID;
+    car.arrival = time(0);
     if(lane==0){
         if((double)rand() / (double)RAND_MAX<1-probability){
+            carID++;
+            car.lane = NORTH;
             northQueue.push(car);
         }
     }else if(lane == 1){
         if((double)rand() / (double)RAND_MAX<probability){
+            carID++;
+            car.lane = EAST;
             eastQueue.push(car);
         }
     }else if(lane == 2){
         if((double)rand() / (double)RAND_MAX<probability){
+            carID++;
+            car.lane = SOUTH;
             southQueue.push(car);
         }
-        
     }else if(lane == 3){
         if((double)rand() / (double)RAND_MAX<probability){
+            carID++;
+            car.lane = WEST;
             westQueue.push(car);
         }
     }
+    pthread_mutex_unlock(&cars);
 }
 
 using namespace std;
 void *defaultLane(void *lane){
+    int carNum = 0;
     while(true){
         long way;
         way = (long)lane;
         pthread_mutex_lock(&street);
-        if(way == 1){
+        if(way == 0){
+            carNum = carID;
             addCar(way);
-            cout << eastQueue.size() << endl;
+            if(carID == carNum){
+                pthread_sleep(19);
+            }
+        }else if(way == 1){
+            addCar(way);
+        }else if(way == 2){
+            addCar(way);
         }else if(way == 3){
             addCar(way);
-            cout << "lane: 3 " << westQueue.size() << endl;
         }
         pthread_mutex_unlock(&street);
         pthread_sleep(1);
@@ -134,7 +184,10 @@ void *police(void*){
     bool laneEmpty = false;
     int max = 0;
     int busyLane = 0;
-    if(max < eastQueue.size()){
+    if (max < northQueue.size()){
+        max = northQueue.size();
+        busyLane = 0;
+    }if(max < eastQueue.size()){
         max = eastQueue.size();
         busyLane = 1;
     }if(max < southQueue.size()){
@@ -143,9 +196,6 @@ void *police(void*){
     }if(max < westQueue.size()){
         max = westQueue.size();
         busyLane = 3;
-    }if (max < northQueue.size()){
-        max = northQueue.size();
-        busyLane = 0;
     }
     int laneNum = busyLane;
     while(true){
@@ -154,8 +204,10 @@ void *police(void*){
         if(laneNum == 0){
             if(northQueue.size() > 0){
                 northQueue.pop();
+                cout<<"car passed from north"<<endl;
                 pthread_sleep(1);
                 if(northQueue.size() == 0){
+                    cout<<"north empty"<<endl;
                     laneEmpty = true;
                 }
             }  
@@ -163,8 +215,10 @@ void *police(void*){
         else if(laneNum == 1){
             if(eastQueue.size() > 0){
                 eastQueue.pop();
+                cout<<"car passed from east"<<endl;
                 pthread_sleep(1);
-                if(northQueue.size() == 0){
+                if(eastQueue.size() == 0){
+                    cout<<"east empty"<<endl;
                     laneEmpty = true;
                 }
             }
@@ -172,8 +226,10 @@ void *police(void*){
         else if(laneNum == 2){
             if(southQueue.size() > 0){
                 southQueue.pop();
+                cout<<"car passed from south"<<endl;
                 pthread_sleep(1);
-                if(northQueue.size() == 0){
+                if(southQueue.size() == 0){
+                    cout<<"south empty"<<endl;
                     laneEmpty = true;
                 }
             }
@@ -181,24 +237,30 @@ void *police(void*){
         else if(laneNum == 3){
             if(westQueue.size() > 0){
                 westQueue.pop();
+                cout<<"car passed from west"<<endl;
                 pthread_sleep(1);
-                if(northQueue.size() == 0){
+                if(westQueue.size() == 0){
                     laneEmpty = true;
+                    cout<<"west empty"<<endl;
                 }
             }
+        }
+        
+        if (max < northQueue.size()){
+            max = northQueue.size();
+            busyLane = 0;
         }
         if(max < eastQueue.size()){
             max = eastQueue.size();
             busyLane = 1;
-        }if(max < southQueue.size()){
+        }
+        if(max < southQueue.size()){
             max = southQueue.size();
             busyLane = 2;
-        }if(max < westQueue.size()){
+        }
+        if(max < westQueue.size()){
             max = westQueue.size();
             busyLane = 3;
-        }if (max < northQueue.size()){
-            max = northQueue.size();
-            busyLane = 0;
         }
         pthread_mutex_unlock(&street);
         if(max >= 5 || laneEmpty){
